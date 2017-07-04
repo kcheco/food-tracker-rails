f97b19677e351026a9f718836cb52cd5be86559f
Kelvin Checo <checokelvin@gmail.com>
2017-07-04 01:46:34 -0400

    Defined tests to run on Meal objects

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Meal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should be valid with valid attributes" do
+
+  end
+
+  it "should require a valid name" do
+
+  end
+
+  it "should require a numerical value for rating" do
+
+  end
+
+  it "should require a rating value greater than or equal to 0" do
+
+  end
+
+  it "should require a rating value less than or equal to 5" do
+
+  end
 end
