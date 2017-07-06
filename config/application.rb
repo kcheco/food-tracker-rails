816bdfe94a0a832cead9a1c7841f01ac89b8902a
Kelvin Checo <checokelvin@gmail.com>
2017-07-06 00:03:39 -0400

    Converted application to API and created an exception handler for controller

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ Bundler.require(*Rails.groups)
 
 module FoodTracker
   class Application < Rails::Application
+    # set application to API
+    config.api_only = true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
