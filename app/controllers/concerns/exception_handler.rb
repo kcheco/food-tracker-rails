e2630ee8741fe0d8b3693df54e046a311c001748
Kelvin Checo <checokelvin@gmail.com>
2017-07-07 06:11:48 -0400

    Implemented a class to handle tokens being passed and tests

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,13 +1,30 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  # Define custom error subclasses - rescue catches `StandardErrors`
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
       render json: { message: e.message }, status: :not_found
     end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      render json: { message: e.message }, status: :unprocessable_entity
-    end
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :four_zero_one
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+  end
+
+  private
+  # JSON response with message and status code 442 - unprocessable entity
+  def four_twenty_two(e)
+    render json: { message: e.message }, status: :unprocessable_entity
+  end
+
+  # JSON response with message and status code 401 - unauthorized
+  def four_zero_one(e)
+    render json: { message: e.message}, status: :unauthorized
   end
 end
